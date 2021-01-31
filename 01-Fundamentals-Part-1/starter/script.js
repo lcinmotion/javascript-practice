c855f6f1ec48a6b4d336b0093a16178ce50cd822
lauren.church <lauren.church@tanium.com>
2021-01-31 11:18:10 -0800

    Initial commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,58 @@
+/*
+// Fundamentals - Part 1 - Values and Variables
+
+// Declaring a variable js with value of amazing and printing to Chrome console
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+// Use camelCase for var names
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// $ and _ are the only symbols allowed in var names
+let $function = 27;
+console.log($function);
+
+let _name = "Lauren";
+console.log(_name);
+
+// Use descriptive variables
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+// Don't use vague naming like this
+let job1 = 'Programmer';
+let job2 = 'Teacher';
+
+// Assignment 1 - LECTURE: Values and Variables
+let country = 'United States';
+let continent = 'North America';
+let population = 3000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+*/
+
+
+// Data Types
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+
+
+// Variables types - let, const, var
